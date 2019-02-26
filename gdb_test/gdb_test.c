e8eadf7f730d571f36ab1cb32fa5f6dfd928b95f
waylon <waylon@localhost.localdomain>
2019-02-26 14:51:03 +0800

    gdb 调试运行进程 attach pid

--- gdb_test/gdb_test.c
+++ gdb_test/gdb_test.c
@@ -8,6 +8,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void getmem(void **p, int num)
+{
+	*p = (void *)malloc(num);
+}
+
+void func(void)
+{
+	char *str = NULL;
+	getmem((void **)&str, 20);
+	strcpy(str, "Hello gdb");
+	printf("%s\n", str);
+	free(str);
+
+}
 
 int main(int argc, char **argv)
 {
@@ -17,5 +34,14 @@ int main(int argc, char **argv)
 	}
 	int n = strtol(argv[1], NULL, 16);
 	printf("%d,%x\n", n, n);
+
+	int quit_sig = 0;
+	while(1){
+		if(quit_sig == 1){
+			func();
+			return 1;
+		}
+		sleep(3);
+	}
 	return 0;
 }
